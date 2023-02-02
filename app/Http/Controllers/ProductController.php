0e3bb1718494439846857952044e88d0226fe7de
eloutassi salma <salma03abb@gmail.com>
2023-02-02 18:05:41 +0100

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function getProducts(){
         $products =Product::all();
-        return response()-> json($products);
+        return response()->json($products);
     }
+
+    public function getCategories(){
+        $categories =Category::all();
+        return response()->json($categories);
+    }
+
+    public function cateProd($id){
+        $category = Category::find($id);
+        return response()->json($category->products);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'getProducts'] );
+Route::get('categories', [ProductController::class, 'getCategories'] );
+Route::get('categories/products/{id}', [ProductController::class, 'cateProd'] );
 
